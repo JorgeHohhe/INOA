8c33562c502e3c24cc108b7a5aa215c32c0aabd4
JorgeHohhe <jorge.costa.profwork@gmail.com>
2023-09-02 13:18:52 -0300

    Add comments and function headers

--- stock-quote-alert.py
+++ stock-quote-alert.py
@@ -1,8 +1,21 @@
 import sys
 
 def validate_parameters(args):
+    """
+    Validates and parses command-line parameters
+
+    Args:
+        args (list): Command-line arguments
+
+    Returns:
+        param1, param2, param3 (tuple): A tuple containing the validated parameters
+        (asset code, bottom price, upper price) if they are valid
+
+        None: If the parameters are invalid or missing
+    """
+    # Check if there are exactly 4 command-line arguments (including the script name)
     if len(args) != 4:
-        print("Usage: python app.py <asset-code> <bottom-price> <upper-price>")
+        print("Usage: python stock-quote-alert.py <asset-code> <bottom-price> <upper-price>")
         return None
 
     try:
@@ -10,16 +23,30 @@ def validate_parameters(args):
         param2 = float(args[2])  # price to sell
         param3 = float(args[3])  # price to buy
     except ValueError:
+         # Handle ValueError if invalid numeric values
         print("Invalid input. Please provide numeric values to prices.")
         return None
 
     return param1, param2, param3
 
 def main():
+    """
+    Entry point of the script to process command-line parameters.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    # Validate the command-line parameters and store them in 'params'
+    para
+    # Validate the command-line parameters
     params = validate_parameters(sys.argv)
     if params is None:
         return
 
+    # Unpack the validated parameters into individual variables
     asset, upper_tunnel, bottom_tunnel = params
 
     print(f"Parameters: {asset} | {upper_tunnel} | {bottom_tunnel}")
